d3b8661eae98d59622e94fbd31db655e731928d8
duct_and_rice <nikuinemitsubishi@gmail.com>
2016-12-15 16:03:04 +0900

    yaruo-extension.user.js追加

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# yaruo-extension
+# やる夫系掲示板用拡張機能
+## インストール
+
+## YouTube埋め込み機能
+    <youtube [VIDEO_ID] [TIME]>
+    例:<youtube LIlZCmETvsY 2:08>
---
+++ yaruo-extension.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         yaruo-extension
+// @namespace    https://github.com/Duct-and-rice/yaruo-extension
+// @version      1.0.0
+// @description  A userscript for Yaruo Cluster
+// @author       The Department of Yaruo of Koushinkyo
+// @match        http://jbbs.shitaraba.net/bbs/read.cgi/*
+// @grant        none
+// ==/UserScript==
+
+(function(){document.body.appendChild(document.createElement('script')).src='https://rawgit.com/Duct-and-rice/yaruo-extension/master/bundle.js';})();
